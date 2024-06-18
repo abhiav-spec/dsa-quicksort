5c87b452f51091144190107c19d06db18e06514c
Abhinav_mishra <kumarabhinav6649@gmail.com>
2024-06-18 09:44:56 +0530

    Add files via upload

---
+++ quicksort.cpp
@@ -0,0 +1,11 @@
+Quicksort Algorithm
+
+Quicksort is a highly efficient sorting algorithm and is based on partitioning of array into smaller subarrays.
+ It follows the divide-and-conquer approach where it:
+
+Selects a "pivot" element from the array.
+
+Partitions the array into two subarrays - elements less than the pivot and elements greater than the pivot.
+Recursively applies the same process to the subarrays.
+This process continues until the entire array is sorted. Quicksort is widely used due to its average-case time complexity of O(n log n), making it one of the fastest sorting algorithms in practice.
+
